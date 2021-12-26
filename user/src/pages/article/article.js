7c6c5eba6e90c81d191634b32fc87951da54d22f
NeirouzJbira <jbira.neirouz@gmail.com>
2021-12-26 18:13:16 +0100

    init

--- user/src/pages/article/article.js
+++ user/src/pages/article/article.js
@@ -1,19 +1,18 @@
-import React from 'react';
-import { useParams } from 'react-router';
-import NavBar from '../../components/navbar';
+import React from "react";
+import { useParams } from "react-router";
+import NavBar from "../../components/navbar";
 
 function Article() {
-    let {id, category} = useParams();
-    return ( 
-        <div>
-            <NavBar/>
-            id={id} and category={category}
-            <div style={{paddingLeft:'5%', paddingTop:1,paddingBottom:50}}>
-                    <h1 style={{color:'#3d3d3d', fontSize:40}}>{category}</h1>
-                    
-            </div>
-        </div>
-     );
+  let { id, category } = useParams();
+  return (
+    <div>
+      <NavBar />
+      id={id} and category={category}
+      <div style={{ paddingLeft: "5%", paddingTop: 1, paddingBottom: 50 }}>
+        <h1 style={{ color: "#3d3d3d", fontSize: 40 }}>{category}</h1>
+      </div>
+    </div>
+  );
 }
 
 export default Article;
