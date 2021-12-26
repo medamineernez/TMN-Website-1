cdd7e80e8b61b8ec851727b823ee9e1681f758c2
NeirouzJbira <jbira.neirouz@gmail.com>
2021-12-26 18:12:08 +0100

    init

--- admin/pages/_app.js
+++ admin/pages/_app.js
@@ -3,9 +3,7 @@ import ReactDOM from "react-dom";
 import App from "next/app";
 import Head from "next/head";
 import Router from "next/router";
-
 import PageChange from "components/PageChange/PageChange.js";
-
 import "assets/plugins/nucleo/css/nucleo.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "assets/scss/nextjs-argon-dashboard.scss";
