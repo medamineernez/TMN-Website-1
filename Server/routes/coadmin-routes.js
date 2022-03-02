2bdd8ec658f14720e897c9cdafb9dbffb28eafee
medamineernez <medamineernez@gmail.com>
2022-03-02 11:48:01 +0100

    kmelet el khedma

--- Server/routes/coadmin-routes.js
+++ Server/routes/coadmin-routes.js
@@ -29,6 +29,35 @@ router.post("/login", (req, res) => {
   });
 });
 
+
+//post a new coadmin 
+
+router.post("/addcoadmin", (req, res) => {
+  const coadmin = new CoAdmin({
+    name: req.body.name,
+    lastName:req.body.lastName,
+    email: req.body.email,
+    password: req.body.password,
+    city : req.body.city,
+    role :"co-admin",
+  });
+
+  coadmin
+    .save()
+    .then(() => {
+      res.status(201).json({
+        message: "coadmin saved successfully !",
+      });
+    })
+    .catch((error) => {
+      res.status(400).json({
+        error: error,
+      });
+    });
+});
+
+
+
 // get all co admins 
 
 router.get("/allCoadmins", (req, res, next) => {
