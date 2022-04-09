4f2975be83707a73a4c4dd8c378c752ac6d576fb
amine.hz <amine.hz.hz.98@gmail.com>
2022-04-09 15:05:32 +0100

    updated

--- user/src/pages/article/article.js
+++ user/src/pages/article/article.js
@@ -2,9 +2,11 @@ import React,{useEffect, useState} from "react";
 import { useParams } from "react-router";
 import Footer from "../../components/footer";
 import NavBar from "../../components/navbar";
-import { Loader, Center } from "@mantine/core";
+import { Loader, Center,Image, SimpleGrid } from "@mantine/core";
 import axios from "axios";
 import ArticleCard from "../../components/articleCard";
+import imagearticle from "../../media/tst.jpg"
+
 
 function Article() {
   let { id, category, subcategory } = useParams();
@@ -13,9 +15,9 @@ function Article() {
   
   useEffect(() => {
     async function getdata(){
-      axios.get("http://localhost:3000/api/blogs/allblogs").then((response) => {
+      axios.get("http://localhost:3000/api/blogs/detail/"+id).then((response) => {
         setPost(response.data);
-        alert(JSON.stringify(post))
+        //alert(JSON.stringify(post))
       });}
     getdata();
     
@@ -23,34 +25,31 @@ function Article() {
 
   if (post){
     
-    post.map((piece) => {
-    rows.push(
-      <ArticleCard
-        title={piece.title}
-        id={piece._id}
-        category={piece.category}
-        description={piece.content}
-        src={piece.image}
-      />
-    );
-    return "";
- });
+
 }
   
  if (!post)
     return (
-      <Center style={{ widt: "100%", height: "80vh" }}>
+      <Center style={{ width: "100%", height: "80vh" }}>
         <Loader color="dark" size="xl" variant="bars" />
       </Center>
     );
     else{
+     
   return (
-    <div>
+    <div style={{height:window.innerHeight,marginBottom:'350px'}}>
       <NavBar />
-      id={id} and category={category} and subcategory={subcategory}
-      <div style={{ height:'100vh' }}>
-        <h1 style={{ color: "#000000", fontSize: 40 }}>{category}</h1>
-        {rows}
+   
+      <div style={{ height:'100vh', width:'80%' , marginLeft:'30px', marginTop:'80px'}}>
+        <h1 style={{ color: "#000000", fontSize: 40 }}>{post.title}</h1>
+        <Image src={imagearticle} alt="dis a pic" style={{width:'40%', float:'right', margin:'20px'}}/>
+        
+        <p style={{textAlign: 'justify',textJustify: 'inter-word', fontSize:'18px'}}>{post.content}</p>
+       
+        <div style={{width:'100%', display:'flex', alignItems:'flex-end',justifyContent:'flex-end'}}>
+        <img src={`data:image/jpeg;base64,${post.image}`} alt="this is indeed an img"/>
+        <p style={{fontFamily: "MonteCarlo, cursive", fontSize:'36px'}}>-{post.author}</p>
+        </div>
       </div>
       <Footer/>
     </div>
